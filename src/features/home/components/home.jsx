f3ccebb0767928abe200b3f2ae3cd43e4e5a1005
nsenji <jerrysyre@gmail.com>
2024-02-01 13:37:49 +0300

    done with top left section for navigation to home and searc

--- src/features/home/components/home.jsx
+++ src/features/home/components/home.jsx
@@ -1,14 +1,28 @@
+import sportify_logo from '../../../assets/Spotify-White-Logo.wine.svg'
+import { FaHome, FaSearch } from 'react-icons/fa';
+
 
 
 export default function Home() {
     return (
-        <section className="bg-black h-screen flex flex-col">
+        <section className="bg-black h-screen flex flex-col px-1 py-1">
 
             <section className="h-87%  flex flex-row">
                 <div className="flex-grow  flex flex-col">
                     <div className=" flex-grow px-1 py-1">
-                        <div className="bg-custom-color h-full rounded-lg">
-
+                        <div className="bg-custom-color h-full rounded-lg  px-2">
+                            <div className=" h-full flex flex-col justify-evenly">
+                                <img className='h-11 w-28' src={sportify_logo} alt="" />
+                                <div className='flex flex-row items-end ml-3'>
+                                    <FaHome className='text-white h-6 w-6'/>
+                                    <p className='text-white font-bold ml-5'>Home</p>
+                                </div>
+                                <div className='flex flex-row items-end ml-3'>
+                                    <FaSearch className='text-gray-600 h-6 w-6'/>
+                                    <p className='text-gray-600 font-bold ml-5'>Search</p>
+                                </div>
+                                
+                            </div>
                         </div>
                     </div>
                     <div className="h-70%  px-1 py-1">
